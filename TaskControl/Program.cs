826e15e026f3415ebb9d86995e142888f9136954
Yuliya Burlachenko <yuliya4015@yandex.ru>
2024-01-14 16:22:04 +0300

    Добавила TaskControl

---
+++ TaskControl/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+ //длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+  //либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+  //лучше обойтись исключительно массивами.
+
+//Примеры:
+//[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+//[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+//[“Russia”, “Denmark”, “Kazan”] → []
+
+class Program
+{
+    //Ввод строки
+    static string[] InputStringArray(string message, char splitter)
+    {
+        Console.WriteLine(message);
+        string? inputString = Console.ReadLine();
+        return inputString.Split(splitter);
+
+    }
+    //Выбор строк длиной меньше N
+    static string[] SelectLenghtLessN(string[] stringArr, int N)
+    {
+        string[] outStringArr = Array.Empty<string>();
+        for (int i = 0; i < stringArr.Length; i++)
+        {
+            if (stringArr[i].Length <= N)
+                outStringArr = outStringArr.Append(stringArr[i]).ToArray();
+        }
+        return outStringArr;
+    }
+
+    //Вывод массива строк
+    static void PrintStringArray(string[] stringArr)
+    {
+        Console.WriteLine(string.Join(", ", stringArr));
+    }
+    static void Main(string[] args)
+    {
+        string[] inStrings = InputStringArray("Введите строки через запятую", ',');
+        string[] outStrings = SelectLenghtLessN(inStrings, 3);
+        PrintStringArray(outStrings);
+
+    }
+}
